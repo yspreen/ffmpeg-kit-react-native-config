2fa5664acc43d9f50db1211216fdce0c9fed3c1f
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-03-19 10:49:41 -0400

    Link to new package.

--- build/withIosDeploymentTarget.js
+++ build/withIosDeploymentTarget.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.withIosDeploymentTarget = void 0;
 exports.updateDeploymentTargetXcodeProject = updateDeploymentTargetXcodeProject;
@@ -9,39 +11,51 @@ exports.updateDeploymentTargetXcodeProject = updateDeploymentTargetXcodeProject;
 const config_plugins_1 = require("expo/config-plugins");
 const semver_1 = __importDefault(require("semver"));
 const withIosDeploymentTarget = (config, props) => {
-    config = withIosDeploymentTargetPodfile(config, props);
-    config = withIosDeploymentTargetXcodeProject(config, props);
-    return config;
+  config = withIosDeploymentTargetPodfile(config, props);
+  config = withIosDeploymentTargetXcodeProject(config, props);
+  return config;
 };
 exports.withIosDeploymentTarget = withIosDeploymentTarget;
 const withIosDeploymentTargetPodfile = (config, props) => {
-    return (0, config_plugins_1.withPodfileProperties)(config, async (config) => {
-        const existing = config.modResults["ios.deploymentTarget"];
-        if (typeof existing !== "string" ||
-            semver_1.default.lt(toSemVer(existing), toSemVer(props.deploymentTarget))) {
-            config.modResults["ios.deploymentTarget"] = props.deploymentTarget;
-        }
-        return config;
-    });
+  return (0, config_plugins_1.withPodfileProperties)(config, async (config) => {
+    const existing = config.modResults["ios.deploymentTarget"];
+    if (
+      typeof existing !== "string" ||
+      semver_1.default.lt(toSemVer(existing), toSemVer(props.deploymentTarget))
+    ) {
+      config.modResults["ios.deploymentTarget"] = props.deploymentTarget;
+    }
+    return config;
+  });
 };
 const withIosDeploymentTargetXcodeProject = (config, props) => {
-    return (0, config_plugins_1.withXcodeProject)(config, (config) => {
-        config.modResults = updateDeploymentTargetXcodeProject(config.modResults, props.deploymentTarget);
-        return config;
-    });
+  return (0, config_plugins_1.withXcodeProject)(config, (config) => {
+    config.modResults = updateDeploymentTargetXcodeProject(
+      config.modResults,
+      props.deploymentTarget
+    );
+    return config;
+  });
 };
 function updateDeploymentTargetXcodeProject(project, deploymentTarget) {
-    const configurations = project.pbxXCBuildConfigurationSection();
-    // @ts-ignore
-    for (const { buildSettings } of Object.values(configurations ?? {})) {
-        const currDeploymentTarget = buildSettings?.IPHONEOS_DEPLOYMENT_TARGET;
-        if (currDeploymentTarget &&
-            semver_1.default.lt(toSemVer(currDeploymentTarget), toSemVer(deploymentTarget))) {
-            buildSettings.IPHONEOS_DEPLOYMENT_TARGET = deploymentTarget;
-        }
+  const configurations = project.pbxXCBuildConfigurationSection();
+  // @ts-ignore
+  for (const { buildSettings } of Object.values(configurations ?? {})) {
+    const currDeploymentTarget = buildSettings?.IPHONEOS_DEPLOYMENT_TARGET;
+    if (
+      currDeploymentTarget &&
+      semver_1.default.lt(
+        toSemVer(currDeploymentTarget),
+        toSemVer(deploymentTarget)
+      )
+    ) {
+      buildSettings.IPHONEOS_DEPLOYMENT_TARGET = deploymentTarget;
     }
-    return project;
+  }
+  return project;
 }
 function toSemVer(version) {
-    return semver_1.default.coerce(version) ?? new semver_1.default.SemVer("0.0.0");
+  return (
+    semver_1.default.coerce(version) ?? new semver_1.default.SemVer("0.0.0")
+  );
 }
